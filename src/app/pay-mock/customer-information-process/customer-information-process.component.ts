5cee7f9d3d62c40928f3b7a1a7d8865402d2a9c0
cofdness <vtkp2002dn@gmail.com>
2022-02-25 14:55:07 +0700

    - html page customer-information-finish

--- src/app/pay-mock/customer-information-process/customer-information-process.component.ts
+++ src/app/pay-mock/customer-information-process/customer-information-process.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customer-information-process',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerInformationProcessComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    setTimeout(()=>{
+      this.router.navigate(['/pay-mock/customer-information-finish']).then()
+    }, 5000)
   }
 
 }
