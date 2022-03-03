3c0369ae30ed8e91c28e7628d63197e4748e1d03
cofdness <vtkp2002dn@gmail.com>
2022-03-03 16:23:07 +0700

    - additional file

---
+++ src/app/pay-mock/customer-information.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {CustomerInformation} from "./customer_infomation";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerInformationService {
+
+  customerInfo: CustomerInformation
+
+  constructor() {
+    this.customerInfo = {
+      name: undefined,
+      sex: undefined,
+      phone: undefined,
+      birthday: undefined,
+      citizenId: undefined,
+      issueDate: undefined,
+
+      city: '',
+      district: '',
+      ward: '',
+      street: '',
+
+      personal_title: '',
+      name_ref: '',
+      phone_ref: ''
+    }
+  }
+}
