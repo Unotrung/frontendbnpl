552ce6869f402bd5cbe5efcfadb80e3505cf38e3
cofdness <vtkp2002dn@gmail.com>
2022-03-16 11:57:54 +0700

    - move tenors -> tenorService

--- src/app/pay-mock/checkout/checkout.component.ts
+++ src/app/pay-mock/checkout/checkout.component.ts
@@ -12,26 +12,7 @@ import {ItemService} from "../item.service";
 })
 export class CheckoutComponent implements OnInit {
 
-  tenors : Tenor[] = [
-    {
-      tenorId: '1',
-      itemPrice: 5000000,
-      convertFee: 1000000,
-      paymentSchedule: 120
-    },
-    {
-      tenorId: '2',
-      itemPrice: 5000000,
-      convertFee: 800000,
-      paymentSchedule: 240
-    },
-    {
-      tenorId: '3',
-      itemPrice: 5000000,
-      convertFee: 500000,
-      paymentSchedule: 120
-    }
-  ]
+  tenors : Tenor[]
   chosenTenor: Tenor | null
   constructor(
       private router: Router,
@@ -40,6 +21,7 @@ export class CheckoutComponent implements OnInit {
       private itemService: ItemService
   ) {
     this.chosenTenor = null
+    this.tenors = this.tenorService.tenors$.getValue()
   }
 
   ngOnInit(): void {
--- src/app/pay-mock/tenor.service.ts
+++ src/app/pay-mock/tenor.service.ts
@@ -6,8 +6,30 @@ import {Tenor} from "./tenor";
   providedIn: 'root'
 })
 export class TenorService {
+
+  tenors$ : BehaviorSubject<Tenor[]>
   selectedTenor$: BehaviorSubject<Tenor | null>
   constructor() {
+    this.tenors$ = new BehaviorSubject<Tenor[]>([
+      {
+        tenorId: '1',
+        itemPrice: 5000000,
+        convertFee: 1000000,
+        paymentSchedule: 120
+      },
+      {
+        tenorId: '2',
+        itemPrice: 5000000,
+        convertFee: 800000,
+        paymentSchedule: 240
+      },
+      {
+        tenorId: '3',
+        itemPrice: 5000000,
+        convertFee: 500000,
+        paymentSchedule: 120
+      }
+    ])
     this.selectedTenor$ = new BehaviorSubject<Tenor | null>(null)
   }
 }
